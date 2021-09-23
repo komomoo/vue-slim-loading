fc52c4a199b1ccdd2eaeacc22c775a37e2e185b8
komo <ko.momo@qq.com>
2021-09-23 11:06:12 +0800

    docs: update demo

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
-// import Loading from '../dist/vue-slim-loading.esm.min.js' // prod
-import Loading from '../src' // dev
+import Loading from '../dist/vue-slim-loading.esm.min.js' // prod
+// import Loading from '../src' // dev
 
 Vue.use(Loading)
 
